4220eacf56d1a5935d225818511d80bfd0d657b0
Abderraouf Zine <rofazayn@gmail.com>
2020-02-23 12:32:43 +0000

    Loaded fonts before render using FFO.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './app.css';
 
 const App = () => {
   return <div>Hello world</div>;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FontFaceObserver from 'fontfaceobserver';
+
+const montserratFont = new FontFaceObserver('Montserrat');
+const openSansFont = new FontFaceObserver('Open Sans');
 
 const rootElement = <App />;
 const rootDiv = document.getElementById('root');
 
-ReactDOM.render(rootElement, rootDiv);
+Promise.all([montserratFont.load(), openSansFont.load()]).then(() =>
+  ReactDOM.render(rootElement, rootDiv)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

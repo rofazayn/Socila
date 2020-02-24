7a1242a8299650a5b4ff401aba5f94e45231648f
Abderraouf Zine <rofazayn@gmail.com>
2020-02-24 17:57:56 +0000

    Coded a basic sign up and sign in views.

--- src/views/SignIn/index.js
+++ src/views/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { Typography } from '@material-ui/core';
 import fb from '../../firebase';
 import { useHistory, Redirect } from 'react-router-dom';
--- src/views/SignUp/index.js
+++ src/views/SignUp/index.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Typography } from '@material-ui/core';
+import fb from '../../firebase';
+import { useHistory, Redirect } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
 
 const SignUp = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const user = await fb
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      console.log(user);
+      history.push('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to='/' />;
+  }
+
   return (
-    <div className='sign-up'>
-      <Typography variant='h1'>Sign up</Typography>
+    <div className='sign-in'>
+      <Typography variant='h1'>Create account</Typography>
+      <form onSubmit={handleSubmit} noValidate>
+        <label htmlFor='email'>
+          Email
+          <input
+            type='email'
+            name='email'
+            value={email}
+            placeholder='You email'
+            onChange={e => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor='password'>
+          Password
+          <input
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Your password'
+            onChange={e => setPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor='password-confirmation'>
+          Password
+          <input
+            type='password'
+            name='passwordConfirmation'
+            value={passwordConfirmation}
+            placeholder='Confirm your password'
+            onChange={e => setPasswordConfirmation(e.target.value)}
+          />
+        </label>
+        <button type='submit'>Create account</button>
+      </form>
     </div>
   );
 };

0fa7ed07e58ffc614e853e704e1b0ce310fef08b
Abderraouf Zine <rofazayn@gmail.com>
2020-02-27 12:46:43 +0000

    Improved page transitions.

--- src/views/SignUp/index.js
+++ src/views/SignUp/index.js
@@ -20,7 +20,7 @@ const SignUp = () => {
       initial={{ opacity: 0, y: 20 }}
       exit={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.2 }}
+      transition={{ duration: 0.5 }}
     >
       <Styled.SignUpView>
         <Container>

961961d129d390cb84e7d3f6934f8d61124740d5
Abderraouf Zine <rofazayn@gmail.com>
2020-03-17 19:12:11 +0000

    Updated auth context to clear user details after logging out.

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -19,6 +19,7 @@ export const AuthProvider = ({ children }) => {
           .then(doc => setUserDetails(doc.data()));
       } else {
         setCurrentUser(false);
+        setUserDetails(null);
       }
     });
     return () => unsubscribeFromAuth();

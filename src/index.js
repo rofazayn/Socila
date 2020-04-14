fafe9632bbf172c43bc209167a90345f4fdc16d2
Abderraouf Zine <rofazayn@gmail.com>
2020-04-14 14:52:05 +0200

    Removed fontfaceobserver loading condition, attepted to fix blank screen on deployment issue.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import FontFaceObserver from 'fontfaceobserver';
+// import FontFaceObserver from 'fontfaceobserver';
 
-const montserratFont = new FontFaceObserver('Montserrat');
-const openSansFont = new FontFaceObserver('Open Sans');
+// const montserratFont = new FontFaceObserver('Montserrat');
+// const openSansFont = new FontFaceObserver('Open Sans');
 
 const rootElement = <App />;
 const rootDiv = document.getElementById('root');
 
-Promise.all([montserratFont.load(), openSansFont.load()]).then(() =>
-  ReactDOM.render(rootElement, rootDiv)
-);
+// Promise.all([montserratFont.load(), openSansFont.load()]).then(() =>
+//   ReactDOM.render(rootElement, rootDiv)
+// );
+
+ReactDOM.render(rootElement, rootDiv);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

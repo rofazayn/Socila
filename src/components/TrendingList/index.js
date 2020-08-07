07ef647bc0e286a93d0f7263eaab09489257a10e
Abderraouf Zine <rofazayn@gmail.com>
2020-08-07 17:32:51 +0100

    Started working on trending list.

---
+++ src/components/TrendingList/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Styled } from './style';
+
+const TrendingList = () => {
+  return <Styled.TrendingList></Styled.TrendingList>;
+};
+
+export default TrendingList;
---
+++ src/components/TrendingList/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const TrendingList = styled.div``;
+
+export const Styled = {
+  TrendingList,
+};

4b237cff94ab1a0509401e77ebe1eb5e1044406f
Abderraouf Zine <rofazayn@gmail.com>
2020-08-10 17:08:03 +0100

    Styled hashtag component on hover.

--- src/components/Hashtag/style.js
+++ src/components/Hashtag/style.js
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom';
 const Hashtag = styled(Link)`
   color: ${({ theme }) => theme.palette.primary.main};
   font-weight: 700;
-
+  transition: all ease-in-out 200ms;
   position: relative;
+  &:hover {
+    /* color: ${({ theme }) => theme.palette.primary.light}; */
+    text-decoration: underline;
+  }
 
   &.--uncolored {
     color: ${({ theme }) => theme.palette.text.primary};

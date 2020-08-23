4ea656f8b883c37c1b9f62c32960a9bd12e8dc22
Abderraouf Zine <rofazayn@gmail.com>
2020-08-23 09:35:04 +0100

    Created a settings box component.

---
+++ src/components/SettingsBox/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styled from './style';
+
+const SettingsBox = ({ children }) => {
+  return <Styled.SettingsBox>{children}</Styled.SettingsBox>;
+};
+
+export default SettingsBox;
---
+++ src/components/SettingsBox/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const SettingsBox = styled.div`
+  max-width: 460px;
+  width: 100%;
+  min-height: 500px;
+  box-shadow: 0 0 15px rgba(black, 0.05);
+`;
+
+export const Styled = {
+  SettingsBox,
+};

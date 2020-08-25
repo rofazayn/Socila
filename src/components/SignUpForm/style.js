c16fac1dc8d60fdeb42d03c28248592c7bfcab5f
Abderraouf Zine <rofazayn@gmail.com>
2020-08-25 18:44:03 +0100

    Got rid of the grid gap on sign up component.

--- src/components/SignUpForm/style.js
+++ src/components/SignUpForm/style.js
@@ -5,7 +5,7 @@ const SignUpForm = styled.div`
   form {
     display: grid;
     grid-column: 1fr;
-    grid-row-gap: 1rem;
+    /* grid-row-gap: 1rem; */
     /* max-width: ${({ theme }) => `${theme.spacing(40)}px`}; */
     .form-controlers {
       display: flex;
@@ -26,5 +26,5 @@ const SignUpForm = styled.div`
 `;
 
 export const Styled = {
-  SignUpForm
+  SignUpForm,
 };

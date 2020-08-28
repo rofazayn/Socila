0b0d654c92a34ea0c2f1935e278268dc1ad55d4f
Abderraouf Zine <rofazayn@gmail.com>
2020-08-28 11:51:59 +0100

    Set up a profile editor component.

---
+++ src/components/ProfileEditor/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Styled } from './style';
+
+const ProfileEditor = () => {
+  return (
+    <Styled.ProfileEditor>
+      <h6>Edit your profile.</h6>
+    </Styled.ProfileEditor>
+  );
+};
+
+export default ProfileEditor;
---
+++ src/components/ProfileEditor/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const ProfileEditor = styled.div``;
+
+export const Styled = { ProfileEditor };
